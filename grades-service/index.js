7c1b52362442ec5813138e6eed32c51292919991
ntua-el21047 <el21047@mail.ntua.gr>
2025-05-07 19:13:50 +0300

    grades

--- grades-service/index.js
+++ grades-service/index.js
@@ -13,7 +13,7 @@ app.post('/grades/upload', upload.single('file'), async (req, res) => {
     try {
         const workbook = xlsx.readFile(req.file.path);
         const sheet = workbook.Sheets[workbook.SheetNames[0]];
-        const data = xlsx.utils.sheet_to_json(sheet, { range: 3 });
+        const data = xlsx.utils.sheet_to_json(sheet, { range: 2 });
 
         for (const row of data) {
             await pool.query(
